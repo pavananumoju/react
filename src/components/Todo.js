f9c3e329d799cedb241c13a4383068aff1f54004
Pavan Kumar Anumoju <pavananumoju@gmail.com>
2023-06-06 23:00:54 +0530

    React with Modal, Props, State

---
+++ src/components/Todo.js
@@ -0,0 +1,28 @@
+import Backdrop from "./Backdrop";
+import Modal from "./Modal";
+import { useState } from "react";
+
+function Todo(props) {
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+
+    function deleteHandler() {
+        setModalIsOpen(true);
+    }
+
+    function closeModalHandler(){
+        setModalIsOpen(false);
+    }
+
+  return (
+    <div className="card">
+      <h2>{props.text}</h2>
+      <div className="actions">
+        <button className="btn" onClick={deleteHandler}>Delete</button>
+      </div>
+      {modalIsOpen ? <Modal onCancel={closeModalHandler} onConfirm={closeModalHandler}/> : null}
+      {modalIsOpen ? <Backdrop onClick={closeModalHandler}/> : null}
+    </div>
+  );
+}
+
+export default Todo;
